83b2ee9527718469c2d5419d2e255a6bf0f31b9e
fumikaobayashi <fumika.obayashi@gmail.com>
2019-06-15 15:14:38 +0900

    Update new and edit
    グループの新規作成・編集機能を実装しました。作成に成功したらフラッシュを表示させました。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,30 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:notice] = "グループを作成しました"
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    group = Group.update(group_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, {user_ids: []})
   end
 end
