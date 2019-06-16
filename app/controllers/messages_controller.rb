b1a1ce5cd7f47c948ff64e807cfdc55f202cc3f2
fumikaobayashi <fumika.obayashi@gmail.com>
2019-06-16 17:31:53 +0900

    Update messages
    投稿機能を実装しました。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,31 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
-    @group = Group.find(params[:group_id])
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      flash[:notice] = "メッセージが送信されました"
+      redirect_to group_messages_path(@group)
+    else
+      @messages = @group.messages.includes(:user)
+      flash[:alert] = "メッセージを入力してください。"
+      # render :index
+      redirect_to group_messages_path(@group)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
